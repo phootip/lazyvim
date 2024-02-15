572ecfd6fd73a92874b3829b3fbc9246c4f0f7bd
phootip <phootip.t@7peakssoftware.com>
2024-02-15 19:04:05 +0700

    add cwd to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,14 @@
 -- stylua: ignore
-if true then return {} end
--- return {
---   "nvim-lualine/lualine.nvim",
---   opts = function(_, opts)
---     table.insert(opts.sections.lualine_c, {
---       require("nvim-possession").status,
---       cond = function()
---         return require("nvim-possession").status() ~= nil
---       end,
---     })
---   end,
--- }
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    table.insert(opts.sections.lualine_b, 1, {
+      require("nvim-possession").status,
+      cond = function()
+        return require("nvim-possession").status() ~= nil
+      end,
+    })
+    -- table.insert(opts.sections.lualine_b, function() return vim.fn.pw end)
+    table.insert(opts.sections.lualine_b, function() return vim.fn.getcwd() end)
+  end,
+}
