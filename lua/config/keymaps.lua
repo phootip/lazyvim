968d833b4850d8ce673fce0dfad01ccaff921b05
phootip <phootip@gmail.com>
2024-02-16 23:11:22 +0700

    add delete without copy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,7 @@ vim.keymap.set("n", "K", "<cmd>bnext<cr>")
 vim.keymap.set("n", "J", "<cmd>bprevious<cr>")
 vim.keymap.set("n", "]<Tab>", "<cmd>tabnext<cr>")
 vim.keymap.set("n", "[<Tab>", "<cmd>tabprevious<cr>")
+
+vim.keymap.set("x", "<leader>p", '"_dP')
+vim.keymap.set({ "n", "x" }, "x", '"_x')
+vim.keymap.set("n", "dx", '"_dd')
