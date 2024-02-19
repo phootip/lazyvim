0aa25b37de8e3a21171bcf11f995d25d527cbdc4
phootip <phootip@gmail.com>
2024-02-19 23:26:50 +0700

    change max_name

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -7,6 +7,16 @@ return {
         style = "underline",
       },
       separator_style = "slant",
+      max_name_length = 25,
+      -- truncate_names = false,
+    },
+    highlights = {
+      buffer_selected = {
+        bold = true,
+        italic = true,
+        -- bold = false,
+        -- italic = false,
+      },
     },
   },
 }
