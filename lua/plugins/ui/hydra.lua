523bfbd5ba94bdc2f970de81e749020bf3218d06
phootip <phootip@gmail.com>
2024-02-20 21:39:30 +0700

    add more key for window management

--- lua/plugins/ui/hydra.lua
+++ lua/plugins/ui/hydra.lua
@@ -7,9 +7,12 @@ return {
   config = function()
     local Hydra = require("hydra")
     Hydra({
-      -- name = "Resize Window",
-      hint = [[Modify Window]],
-      config = { color = "pink", hint = { border = "double" } },
+      name = "Window Operation",
+      hint = [[Window Operation]],
+      config = {
+        color = "red",
+        hint = { border = "double" },
+      },
       mode = "n",
       body = "<leader>w",
       heads = {
@@ -17,6 +20,14 @@ return {
         { "=", require("smart-splits").resize_up, { desc = "Window height down" } },
         { ".", require("smart-splits").resize_right, { desc = "Decrease window width" } },
         { ",", require("smart-splits").resize_left, { desc = "Increase window width" } },
+
+        { "b", "<cmd>BufferLineMoveNext<cr>", { desc = "Buffer move right" } },
+        { "B", "<cmd>BufferLineMovePrev<cr>", { desc = "Buffer move left" } },
+        { "t", "<cmd>tabnext<cr>", { desc = "Tab Next" } },
+        { "T", "<cmd>tabprevious<cr>", { desc = "Tab previous" } },
+        { "r", "<cmd>+tabmove<cr>", { desc = "Tab move right" } },
+        { "R", "<cmd>-tabmove<cr>", { desc = "Tab move left" } },
+        { "D", "<cmd>-tabclose<cr>", { desc = "Tab close" } },
       },
     })
     vim.keymap.set("n", "<leader>w_", "<C-W>s", { desc = "Split window below", remap = true })
