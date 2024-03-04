2c549cb4c071b293e84b69e6571f1f7c880cde02
phootip <phootip.t@7peakssoftware.com>
2024-03-04 17:51:20 +0700

    add lang terraform

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.editor.symbols-outline" },
     -- { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
     -- { import = "lazyvim.plugins.extras.editor.navic" },
     -- import/override with your plugins
     { import = "plugins" },
