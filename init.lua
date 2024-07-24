ef717acb2aa51b15199f6da2ca800617d8543bbb
phootip <phootip.t@gmail.com>
2024-07-24 17:33:22 +0700

    test creating your own plugin

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.termap")
+require("config.termap").init_highlights()
+require("config.termap").run_autocommands()
