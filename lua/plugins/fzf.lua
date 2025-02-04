9d364c571532c7ecfb1cb9d8b76ad93fb9ea6b96
phootip <phootip.t@gmail.com>
2025-02-04 11:07:31 +0700

    fzf use vertical preview

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -9,6 +9,14 @@ return {
         syntax_limit_b = 1024 * 100,
       },
     },
+    winopts = {
+      fullscreen = true,
+
+      preview = {
+        layout = "vertical",
+        vertical = "up:45%",
+      },
+    },
     keymap = {
       fzf = {
         -- use cltr-q to select all items and convert to quickfix list
