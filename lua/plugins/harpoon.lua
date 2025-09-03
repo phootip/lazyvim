0569db08ec07ec50a631565560eccc50471b069d
phootip <phootip.t@gmail.com>
2025-09-03 15:10:33 +0700

    map alt-hjkl to change pane

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,10 +12,10 @@ return {
       buffer_leader_key = 'm', -- Per Buffer Mappings
     },
     keys = {
-      { "<M-h>", function() require("arrow.persist").go_to(1) end },
-      { "<M-j>", function() require("arrow.persist").go_to(2) end },
-      { "<M-k>", function() require("arrow.persist").go_to(3) end },
-      { "<M-l>", function() require("arrow.persist").go_to(4) end },
+      -- { "<M-h>", function() require("arrow.persist").go_to(1) end },
+      -- { "<M-j>", function() require("arrow.persist").go_to(2) end },
+      -- { "<M-k>", function() require("arrow.persist").go_to(3) end },
+      -- { "<M-l>", function() require("arrow.persist").go_to(4) end },
     }
   }
 }
