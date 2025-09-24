05ebe5d7ad6a7260735a38936e73a8692f7d7725
phootip <phootip.t@gmail.com>
2025-09-24 13:51:40 +0700

    add avante.nvim

--- lua/plugins/editor/ai.lua
+++ lua/plugins/editor/ai.lua
@@ -1,25 +1,89 @@
 return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = {
-    strategies = {
-      chat = {
-        adapter = "gemini",
-        keymaps = {
-          close = {
-            modes = { n = "<C-F14>", i = "<C-F14>" },
+  -- "olimorris/codecompanion.nvim",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   "nvim-treesitter/nvim-treesitter",
+  -- },
+  -- config = {
+  --   strategies = {
+  --     chat = {
+  --       adapter = "gemini",
+  --       keymaps = {
+  --         close = {
+  --           modes = { n = "<C-F14>", i = "<C-F14>" },
+  --         },
+  --       },
+  --     },
+  --     inline = {
+  --       adapter = "gemini",
+  --     },
+  --   },
+  -- },
+  -- keys = {
+  --   { "<C-a>", "<CMD>CodeCompanionActions<CR>", silent = true, mode = { "n" }, desc = "CodeCompanionActions" },
+  -- },
+  {
+    "yetone/avante.nvim",
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      or "make",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      -- add any opts here
+      -- this file can contain specific instructions for your project
+      instructions_file = "avante.md",
+      -- for example
+      provider = "gemini",
+      input = {
+        provider = "snacks",
+        provider_opts = {
+          -- Additional snacks.input options
+          title = "Avante Input",
+          icon = " ",
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "stevearc/dressing.nvim", -- for input provider dressing
+      "folke/snacks.nvim", -- for input provider snacks
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
           },
         },
       },
-      inline = {
-        adapter = "gemini",
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
       },
     },
-  },
-  keys = {
-    { "<C-a>", "<CMD>CodeCompanionActions<CR>", silent = true, mode = { "n" }, desc = "CodeCompanionActions" },
   },
 }
