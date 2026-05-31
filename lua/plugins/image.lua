7e1ba2a5ba7234ba1ef6f06eee0c27cc6257ee61
phootip.t <phootip.t@gmail.com>
2026-05-31 14:27:27 +0700

    add images

---
+++ lua/plugins/image.lua
@@ -0,0 +1,24 @@
+-- NOTE: render images inline in terminal (kitty/wezterm/ghostty graphics protocol)
+return {
+  {
+    "3rd/image.nvim",
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      processor = "magick_cli",
+      tmux_show_only_in_active_window = true,
+    },
+  },
+  -- NOTE: paste images from clipboard, save to disk, insert markdown link
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add options here
+      -- or leave it empty to use the default settings
+    },
+    keys = {
+      -- suggested keymap
+      { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
+  },
+}
