75f9b331cffa9419bfb0dcff55f3d897aadcf862
phootip.t <phootip.t@gmail.com>
2026-07-13 13:44:47 +0700

    add antigravity

--- lua/plugins/editor/ai.lua
+++ lua/plugins/editor/ai.lua
@@ -45,6 +45,9 @@ return {
           amazon_q = {
             cmd = { "kiro-cli", "--agent", "c" },
           },
+          antigravity = {
+            cmd = { "agy", "-c" },
+          },
           kiro = {
             cmd = { "kiro-cli", "--agent", "c" },
             -- format = function(text)
